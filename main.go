3be91a682e2f099fb8a35c92cd977fa263738378
r0thko <74073424+r0thko@users.noreply.github.com>
2026-06-04 18:13:13 +0200

    add: colorize soon-to-expire dates

--- main.go
+++ main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"crypto/tls"
-	"errors"
 	"fmt"
 	"net"
 	"os"
 	"strings"
 	"time"
+)
+
+const (
+	ColorReset  = "\033[0m"
+	ColorRed    = "\033[31m"
+	ColorYellow = "\033[33m"
+	Bold        = "\033[1m"
 )
 
 func main() {
@@ -19,7 +25,8 @@ func main() {
 
 	endpoints, err = NormalizeEndpoints(endpoints)
 	if err != nil {
-		errors.New("could not normalize endpoints.list: " + err.Error())
+		fmt.Println("Could not normalize endpoints.list:", err)
+		return
 	}
 
 	for _, endpoint := range endpoints {
@@ -33,7 +40,7 @@ func main() {
 }
 
 func NormalizeEndpoints(endpoints []string) ([]string, error) {
-	normalizedEndpoints := make([]string, len(endpoints))
+	normalizedEndpoints := make([]string, 0, len(endpoints))
 	for _, endpoint := range endpoints {
 		if endpoint == "" {
 			continue
@@ -50,7 +57,7 @@ func NormalizeEndpoints(endpoints []string) ([]string, error) {
 }
 
 func checkCertificate(endpoint string) {
-	host, port, err := net.SplitHostPort(endpoint)
+	host, _, err := net.SplitHostPort(endpoint)
 	if err != nil {
 		fmt.Printf("%s -> ERROR: invalid endpoint\n", endpoint)
 		return
@@ -66,13 +73,30 @@ func checkCertificate(endpoint string) {
 	defer conn.Close()
 	cert := conn.ConnectionState().PeerCertificates[0]
 	daysRemaining := int(time.Until(cert.NotAfter).Hours() / 24)
+	status := fmt.Sprintf("%d days left", daysRemaining)
+
+	if daysRemaining < 15 {
+		status = fmt.Sprintf("%s%s%s%s",
+			Bold,
+			ColorRed,
+			status,
+			ColorReset,
+		)
+	} else if daysRemaining < 30 {
+		status = fmt.Sprintf("%s%s%s%s",
+			Bold,
+			ColorYellow,
+			status,
+			ColorReset,
+		)
+	}
+
 	fmt.Printf(
-		"%s:%s | %s | expires: %s | %d days left\n",
-		host,
-		port,
+		"%-25s | %-25s | %-12s | %s\n",
+		endpoint,
 		cert.Subject.CommonName,
 		cert.NotAfter.Format("2006-01-02"),
-		daysRemaining,
+		status,
 	)
 
 }
