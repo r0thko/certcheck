1ba6a3ee799e547c3a2dacdb8062fcebff069d8c
r0thko <74073424+r0thko@users.noreply.github.com>
2026-06-04 19:18:15 +0200

    edit: refactoring

--- main.go
+++ main.go
@@ -35,23 +35,30 @@ type CertInfo struct {
 	Severity   Severity
 }
 
-var results []CertInfo
-
 func main() {
-	data, err := os.ReadFile("endpoints.list")
-	if err != nil {
-		panic(err)
+	var results []CertInfo
+
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: certcheck <endpoints-file>")
+		os.Exit(1)
 	}
-	endpoints := strings.Split(string(data), "\n")
+
+	file := os.Args[1]
+
+	endpoints, err := LoadEndpoints(file)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
 
 	endpoints, err = NormalizeEndpoints(endpoints)
 	if err != nil {
-		fmt.Println("Could not normalize endpoints.list:", err)
-		return
+		fmt.Println("Error:", err)
+		os.Exit(1)
 	}
 
 	for _, endpoint := range endpoints {
-		result, err := checkCertificate(endpoint)
+		result, err := GetCertificateInfo(endpoint)
 		if err != nil {
 			continue
 		}
@@ -59,6 +66,74 @@ func main() {
 		results = append(results, result)
 	}
 
+	PrintResults(results)
+}
+
+func LoadEndpoints(path string) ([]string, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	endpoints := strings.Split(string(data), "\n")
+	return endpoints, nil
+}
+
+func NormalizeEndpoints(endpoints []string) ([]string, error) {
+	normalizedEndpoints := make([]string, 0, len(endpoints))
+	for _, endpoint := range endpoints {
+		if endpoint == "" {
+			continue
+		}
+
+		endpoint = strings.TrimSpace(endpoint)
+
+		if !strings.Contains(endpoint, ":") {
+			endpoint += ":443"
+		}
+		normalizedEndpoints = append(normalizedEndpoints, endpoint)
+	}
+	return normalizedEndpoints, nil
+}
+
+func GetCertificateInfo(endpoint string) (CertInfo, error) {
+	host, _, err := net.SplitHostPort(endpoint)
+	if err != nil {
+		fmt.Printf("%s -> ERROR: invalid endpoint\n", endpoint)
+		return CertInfo{}, err
+	}
+
+	conn, err := tls.Dial("tcp", endpoint, &tls.Config{
+		ServerName: host,
+	})
+	if err != nil {
+		fmt.Printf("%s -> ERROR: %v\n", host, err)
+		return CertInfo{}, err
+	}
+	defer conn.Close()
+	cert := conn.ConnectionState().PeerCertificates[0]
+	daysRemaining := int(time.Until(cert.NotAfter).Hours() / 24)
+	status := fmt.Sprintf("%d days left", daysRemaining)
+
+	severity := SeverityOK
+
+	if daysRemaining < CriticalTime {
+		severity = SeverityCritical
+	} else if daysRemaining < WarningTime {
+		severity = SeverityWarning
+	}
+
+	return CertInfo{
+		Endpoint:   endpoint,
+		CommonName: cert.Subject.CommonName,
+		Expires:    cert.NotAfter.Format("2006-01-02"),
+		Status:     status,
+		DaysLeft:   daysRemaining,
+		Severity:   severity,
+	}, nil
+
+}
+
+func PrintResults(results []CertInfo) {
 	endpointWidth := len("ENDPOINT")
 	commonNameWidth := len("COMMON NAME")
 	expiresWidth := len("EXPIRES")
@@ -125,59 +200,4 @@ func main() {
 			result.Severity,
 		)
 	}
-}
-
-func NormalizeEndpoints(endpoints []string) ([]string, error) {
-	normalizedEndpoints := make([]string, 0, len(endpoints))
-	for _, endpoint := range endpoints {
-		if endpoint == "" {
-			continue
-		}
-
-		endpoint = strings.TrimSpace(endpoint)
-
-		if !strings.Contains(endpoint, ":") {
-			endpoint += ":443"
-		}
-		normalizedEndpoints = append(normalizedEndpoints, endpoint)
-	}
-	return normalizedEndpoints, nil
-}
-
-func checkCertificate(endpoint string) (CertInfo, error) {
-	host, _, err := net.SplitHostPort(endpoint)
-	if err != nil {
-		fmt.Printf("%s -> ERROR: invalid endpoint\n", endpoint)
-		return CertInfo{}, err
-	}
-
-	conn, err := tls.Dial("tcp", endpoint, &tls.Config{
-		ServerName: host,
-	})
-	if err != nil {
-		fmt.Printf("%s -> ERROR: %v\n", host, err)
-		return CertInfo{}, err
-	}
-	defer conn.Close()
-	cert := conn.ConnectionState().PeerCertificates[0]
-	daysRemaining := int(time.Until(cert.NotAfter).Hours() / 24)
-	status := fmt.Sprintf("%d days left", daysRemaining)
-
-	severity := SeverityOK
-
-	if daysRemaining < CriticalTime {
-		severity = SeverityCritical
-	} else if daysRemaining < WarningTime {
-		severity = SeverityWarning
-	}
-
-	return CertInfo{
-		Endpoint:   endpoint,
-		CommonName: cert.Subject.CommonName,
-		Expires:    cert.NotAfter.Format("2006-01-02"),
-		Status:     status,
-		DaysLeft:   daysRemaining,
-		Severity:   severity,
-	}, nil
-
 }
